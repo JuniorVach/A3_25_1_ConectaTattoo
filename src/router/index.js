804079aa3069c5ab5d0d97081db96a4aa048e9c1
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-10 11:57:24 -0300

    fix:router path

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../pages/HomeView.vue'
+import IndexPage from '../pages/IndexPage.vue'
+import FemininoPage from '../pages/FemininoPage.vue'
+import MasculinoPage from '../pages/MasculinoPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-    },
+    path: '/',
+    component: HomeView,
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: IndexPage,
+      },
+      {
+        path: 'categorias/feminino',
+        name: 'categoriaFeminino',
+        component: FemininoPage,
+      },
+      {
+        path: 'categorias/masculino',
+        name: 'categoriaMasculino',
+        component: MasculinoPage,
+      },
+    ],
+  },
   ],
 })
 
