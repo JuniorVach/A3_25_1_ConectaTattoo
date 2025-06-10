f908047966635ad12be6bca78f0e6d3457f37990
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-10 11:58:47 -0300

    update navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,86 +1,58 @@
 <template>
-  <q-layout view="hHh lpR fFf">
-    <q-header elevated class="bg-dark text-white">
-      <q-toolbar>
-        <!-- <q-btn flat dense round icon="menu" @click="toggleLeftDrawer" class="q-mr-sm" /> -->
+  <q-header elevated class="bg-dark text-white">
+    <q-toolbar>
+      <q-toolbar-title>
+        <router-link to="/" class="toolbar-link">
+          <q-img :src="LogoTattoo" class="logo-tattoo" />
+          <span class="title">Conecta Tattoo</span>
+        </router-link>
+      </q-toolbar-title>
 
-        <q-toolbar-title>
-          <router-link to="/" class="toolbar-link">
-            <q-img :src="LogoTattoo" class="logo-tattoo" />
-            <span class="title">Conecta Tattoo</span>
-          </router-link>
-        </q-toolbar-title>
+      <q-btn flat label="Home" to="/" />
+      <q-btn-dropdown flat label="Categorias">
+        <q-list>
+          <q-item clickable v-close-popup @click="goToCategoria('masculino')">
+            <q-item-section>
+              <q-item-label>Masculino</q-item-label>
+            </q-item-section>
+          </q-item>
 
-        <q-btn flat label="Home" to="/" />
-        <!-- <q-btn flat label="Artes" to="/artes" /> -->
-        <q-btn-dropdown flat label="Categorias">
-          <q-list>
-            <q-item clickable v-close-popup @click="goToCategoria('masculino')">
-              <q-item-section>
-                <q-item-label>Masculino</q-item-label>
-              </q-item-section>
+          <q-item clickable v-close-popup @click="goToCategoria('feminino')">
+            <q-item-section>
+              <q-item-label>Feminino</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+
+      <q-btn flat round icon="account_circle">
+        <q-menu>
+          <q-list style="min-width: 150px">
+            <q-item clickable v-close-popup>
+              <q-item-section>Action</q-item-section>
             </q-item>
-
-            <q-item clickable v-close-popup @click="goToCategoria('feminino')">
-              <q-item-section>
-                <q-item-label>Feminino</q-item-label>
-              </q-item-section>
+            <q-item clickable v-close-popup>
+              <q-item-section>Another Action</q-item-section>
+            </q-item>
+            <q-separator />
+            <q-item clickable v-close-popup>
+              <q-item-section>Something else here</q-item-section>
             </q-item>
           </q-list>
-        </q-btn-dropdown>
+        </q-menu>
+      </q-btn>
 
-        <q-btn flat round icon="account_circle">
-          <q-menu>
-            <q-list style="min-width: 150px">
-              <q-item clickable v-close-popup>
-                <q-item-section>Action</q-item-section>
-              </q-item>
-              <q-item clickable v-close-popup>
-                <q-item-section>Another Action</q-item-section>
-              </q-item>
-              <q-separator />
-              <q-item clickable v-close-popup>
-                <q-item-section>Something else here</q-item-section>
-              </q-item>
-            </q-list>
-          </q-menu>
-        </q-btn>
-
-        <q-space />
-
-        <!-- <q-input
-          dense
-          filled
-          debounce="300"
-          v-model="search"
-          placeholder="Search"
-          class="q-mr-sm"
-        /> -->
-      </q-toolbar>
-    </q-header>
-
-    <!-- Aqui vai o conteúdo das páginas -->
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+      <q-space />
+    </q-toolbar>
+  </q-header>
 </template>
 
 <script setup>
-// import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import LogoTattoo from '../assets/logo_conecta-tattoo.png'
 
-// const search = ref('')
 const router = useRouter()
 
-// function onSearch() {
-//   console.log('Searching:', search.value)
-// }
-
-//function toggleLeftDrawer() {
-// Função para abrir o menu lateral (se necessário)
-//}
 function goToCategoria(tipo) {
   router.push(`/categorias/${tipo}`)
 }
