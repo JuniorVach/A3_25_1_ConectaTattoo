444c2e8ebbe2d805c7a62a7b3d6f6c1283ea5bf4
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-11 20:39:30 -0300

    atualizando rotas para pagina tatuador

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,6 +2,7 @@ import MainLayout from 'layouts/MainLayout.vue';
 import HomeView from 'pages/HomeView.vue';
 import FemininoPage from 'pages/FemininoPage.vue';
 import MasculinoPage from 'pages/MasculinoPage.vue';
+import TatuadorAuthPage from 'pages/TatuadorAuthPage.vue';
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
       { path: 'categorias/feminino', name: 'categoriaFeminino', component: FemininoPage },
       { path: 'categorias/masculino', name: 'categoriaMasculino', component: MasculinoPage },
     ],
+  },
+  {
+    path: '/tatuador/auth',
+    name: 'tatuadorAuth',
+    component: TatuadorAuthPage,
   },
 
   // Always leave this as last one,
